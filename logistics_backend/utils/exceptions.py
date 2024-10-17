e952bfe3c3cfafc71627f8c57882a2edc2215c38
HistoriFy <rathorehotandcool2002@gmail.com>
2024-10-18 04:35:02 +0530

    exception handler custom message bug fix

--- logistics_backend/utils/exceptions.py
+++ logistics_backend/utils/exceptions.py
@@ -2,15 +2,22 @@ from rest_framework import status
 
 
 class CustomException(Exception):
+    def __init__(self, message=None):
+        if message is None:
+            message = self.default_message
+            
+        self.message = message
+        super().__init__(self.message)
+    
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-    message = 'An error occured'
+    default_message = 'An error occured'
 
 
 class BadRequest(CustomException):
     status_code = status.HTTP_400_BAD_REQUEST
-    message = 'The request is invalid or malformed'
+    default_message = 'The request is invalid or malformed'
     
     
 class Unauthorized(CustomException):
     status_code = status.HTTP_401_UNAUTHORIZED
-    message = 'You are not authorized to access this resource'
+    default_message = 'You are not authorized to access this resource'
