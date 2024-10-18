a89778f8453db08f0e770a3b8920f635be9d4b52
HistoriFy <rathorehotandcool2002@gmail.com>
2024-10-18 07:17:06 +0530

    redundant api access removed

--- logistics_backend/vehicle_type/views.py
+++ logistics_backend/vehicle_type/views.py
@@ -2,12 +2,11 @@ from rest_framework.views import APIView
 
 from .models import VehicleType
 
-from utils.custom_jwt_auth import CustomJWTAuthentication, IsDriver, IsFleetOwner, IsRegularUser
+from utils.custom_jwt_auth import CustomJWTAuthentication
 from utils.helpers import format_response
 
 class GetVehicleTypeNamesView(APIView):
     authentication_classes = [CustomJWTAuthentication]
-    permission_classes = [IsDriver, IsFleetOwner, IsRegularUser]
     
     @format_response
     def get(self, request):
