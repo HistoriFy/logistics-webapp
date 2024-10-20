52a499c4bf41c93ea16552d811892f42d188b1b3
HistoriFy <rathorehotandcool2002@gmail.com>
2024-10-21 03:10:51 +0530

    added more driver info in bookings update

--- logistics_backend/booking/signals.py
+++ logistics_backend/booking/signals.py
@@ -3,6 +3,7 @@ from django.dispatch import receiver
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
+from authentication.models import Driver
 from .models import Booking
 from booking.serializers import BookingSerializer
 
@@ -12,6 +13,15 @@ def send_booking_status_update(sender, instance, created, **kwargs):
     if not created:
         channel_layer = get_channel_layer()
         group_name = f"user_{instance.user.id}_bookings"
+        
+        if instance.driver:
+            driver_name = Driver.objects.get(id=instance.driver_id).name
+            driver_rating = Driver.objects.get(id=instance.driver_id).rating
+            driver_phone_number = Driver.objects.get(id=instance.driver_id).phone
+            
+            serializer.data["driver_phone_number"] = driver_phone_number
+            serializer.data["driver_name"] = driver_name
+            serializer.data["driver_rating"] = driver_rating
 
         serializer = BookingSerializer(instance)
         message = {
